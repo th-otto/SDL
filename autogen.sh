84d849d6f0a13d48d2f35eaedaaed9fa15f8c495
Thorsten Otto <admin@tho-otto.de>
2017-08-22 15:18:15 +0200

    Update autoconf scripts

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,15 @@ echo "Generating build information using autoconf"
 echo "This may take a while ..."
 
 # Regenerate configuration files
-cat acinclude/* >aclocal.m4
+found=false
+for aclocal in aclocal-1.15 aclocal-1.14 aclocal
+do if which $aclocal >/dev/null 2>&1; then $aclocal -I acinclude && found=true; break; fi
+done
+if test x$found = xfalse; then
+    echo "Couldn't find aclocal, aborting"
+    exit 1
+fi
+
 found=false
 for autoconf in autoconf autoconf259 autoconf-2.59
 do if which $autoconf >/dev/null 2>&1; then $autoconf && found=true; break; fi
@@ -13,7 +21,11 @@ if test x$found = xfalse; then
     echo "Couldn't find autoconf, aborting"
     exit 1
 fi
-(cd test; sh autogen.sh)
+(cd test; sh autogen.sh; rm -rf autom4te.cache)
+
+libtoolize -i -c
+
+rm -rf autom4te.cache
 
 # Run configure for this platform
 echo "Now you are ready to run ./configure"
